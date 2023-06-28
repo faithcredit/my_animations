bfd17a0a35e4ae1545948e2422e3afa2b4421c4e
faithcredit <honest.dedicate@gmail.com>
2023-06-29 04:07:03 +0900

    shape animation usein animation controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'my_animation.dart';
+import './shape_animation.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-     home: MyAnimation(),
+     home: const ShapeAnimation(),
    );
   }
 }
