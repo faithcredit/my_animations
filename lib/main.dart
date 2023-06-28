01a4c41c1ad75c8e59cab27ba2eb2cabdb2eee62
faithcredit <honest.dedicate@gmail.com>
2023-06-29 04:49:57 +0900

    animation Using Hero

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'my_animation.dart';
 import './shape_animation.dart';
+import './list_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-     home: const ShapeAnimation(),
+     home: ListScreen(),
    );
   }
 }
